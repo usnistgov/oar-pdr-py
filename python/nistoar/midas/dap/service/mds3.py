d51a695f6ab10dc05bd9be55dfe0a4f848a5e07b
RayPlante <raymond.plante@nist.gov>
2023-11-30 10:37:30 -0500

    dap/mds3: include authors as among allowed attributes in a reference

--- python/nistoar/midas/dap/service/mds3.py
+++ python/nistoar/midas/dap/service/mds3.py
@@ -1732,7 +1732,8 @@ class DAPService(ProjectService):
         return auth
 
     _refprops = set(("@id _schema _extensionSchemas title abbrev proxyFor location label "+
-                     "description citation refType doi inPreparation vol volNumber pages publishYear").split())
+                     "description citation refType doi inPreparation vol volNumber pages "+
+                     "authors publishYear").split())
     _reftypes = set(("IsDocumentedBy IsSupplementTo IsSupplementedBy IsCitedBy Cites IsReviewedBy "+
                      "IsReferencedBy References IsSourceOf IsDerivedFrom "+
                      "IsNewVersionOf IsPreviousVersionOf").split())
