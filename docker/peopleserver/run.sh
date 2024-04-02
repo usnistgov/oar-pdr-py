8f595e00cd8b945390e5a77c82a099ba619a75b6
RayPlante <raymond.plante@nist.gov>
2024-04-02 06:52:20 -0400

    add docker peopleserver

---
+++ docker/peopleserver/run.sh
@@ -0,0 +1,247 @@
+#! /bin/bash
+#
+# run.sh -- launch the server in a docker container
+#
+
+prog=peopleserver
+execdir=`dirname $0`
+[ "$execdir" = "" -o "$execdir" = "." ] && execdir=$PWD
+dockerdir=`(cd $execdir/.. > /dev/null 2>&1; pwd)`
+repodir=`(cd $dockerdir/.. > /dev/null 2>&1; pwd)`
+scriptsdir=$repodir/scripts
+os=`uname`
+SED_RE_OPT=r
+[ "$os" != "Darwin" ] || SED_RE_OPT=E
+
+PACKAGE_NAME=oar-pdr-py
+DEFAULT_CONFIGFILE=$dockerdir/peopleserver/people_conf.yml
+
+set -e
+
+function usage {
+    cat <<EOF
+$prog - launch a docker container running the NSD web server
+
+SYNOPSIS
+  $prog [-h|--help] [-b|--build] [-D|--docker-build] [-c|--config-file FILE] 
+              [DIR] [start|stop]
+
+ARGUMENTS
+  start                         Start the service; this is the default if the 
+                                start|stop argument is not provided.  
+  stop                          Stop the running service.  If -M was used to 
+                                start the service, it must also provided when
+                                stopping it.  
+  DIR                           a directory where the database data backing the 
+                                server will be stored.  If not provided, a 
+                                temporary directory within the peopleserver 
+                                container will be used.  Provide this if you want
+                                look at the database contents directly.
+  -b, --build                   Rebuild the python library and install into dist;
+                                This is done automatically if the dist directory 
+                                does not exist.
+  -D, --docker-build            Rebuild the peopleserver docker image; this is 
+                                done automatically if the peopleserver image 
+                                does not exist.
+  -c FILE, --config-file FILE   Use a custom service configuration given in FILE.
+                                This file must be in YAML or JSON format.
+                                Defaut: docker/peopleserver/people_config.yml
+  -B, --bg                      Run the server in the background (returning the 
+                                command prompt after successful launch)
+  -N, --no-mongo                Do not start a mongo server; use this if you want
+                                to use a server already running (e.g. as part of 
+                                midasserver).
+  -p, --port NUM                The port that the service should listen to 
+                                (default: 9091)
+  -h, --help                    Print this text to the terminal and then exit
+
+EOF
+}
+
+function docker_images_built {
+    for image in "$@"; do
+        (docker images | grep -qs $image) || {
+            return 1
+        }
+    done
+    return 0
+}
+
+function build_server_image {
+    echo '+' $dockerdir/dockbuild.sh -d peopleserver
+    $dockerdir/dockbuild.sh -d peopleserver # > log
+}
+
+PORT=9092
+DOPYBUILD=
+DODOCKBUILD=
+CONFIGFILE=
+USEMONGO=1
+STOREDIR=
+DETACH=
+while [ "$1" != "" ]; do
+    case "$1" in
+        -b|--build)
+            DOPYBUILD="-b"
+            ;;
+        -D|--docker-build)
+            DODOCKBUILD="-D"
+            ;;
+        -c)
+            shift
+            CONFIGFILE=$1
+            ;;
+        --config-file=*)
+            CONFIGFILE=`echo $1 | sed -e 's/[^=]*=//'`
+            ;;
+        -B|--bg|--detach)
+            DETACH="--detach"
+            ;;
+        -N|--no-mongo)
+            USEMONGO=
+            ;;
+        -p)
+            shift
+            PORT=$1
+            ;;
+        --port=*)
+            PORT=`echo $1 | sed -e 's/[^=]*=//'`
+            ;;
+        -h|--help)
+            usage
+            exit
+            ;;
+        -*)
+            echo "${prog}: unsupported option:" $1
+            false
+            ;;
+        start|stop)
+            [ -z "$ACTION" ] || {
+                echo "${prog}: Action $ACTION already set; provide only one"
+                false
+            }
+            ACTION=`echo $1 | tr A-Z a-z`
+            ;;
+        *)
+            [ -z "$STOREDIR" ] || {
+                echo "${prog}: DIR already set to $STOREDIR; unsupported extra argument:" $1
+                false
+            }
+            STOREDIR=$1
+            ;;
+    esac
+    shift
+done
+[ -n "$ACTION" ] || ACTION=start
+
+([ -z "$DOPYBUILD" ] && [ -e "$repodir/dist/pdr" ]) || {
+    echo '+' scripts/install.sh --prefix=$repodir/dist/pdr
+    $repodir/scripts/install.sh --prefix=$repodir/dist/pdr
+}
+[ -d "$repodir/dist/pdr/lib/python/nistoar" ] || {
+    echo ${prog}: Python library not found in dist directory: $repodir/dist
+    false
+}
+VOLOPTS="-v $repodir/dist:/app/dist"
+VOLOPTS="$VOLOPTS -v $repodir/scripts/people-uwsgi.py:/dev/oar-pdr-py/scripts/people-uwsgi.py"
+
+datadir=$repodir/docker/data
+ls $datadir/*.json > /dev/null 2>&1 || {
+    # no JSON data found in datadir; reset the datadir to test data
+    datadir=$repodir/python/tests/nistoar/nsd/data
+}
+VOLOPTS="$VOLOPTS -v ${datadir}:/app/data"
+
+# build the docker images if necessary
+(docker_images_built peopleserver && [ -z "$DODOCKBUILD" ]) || build_server_image
+
+[ -n "$CONFIGFILE" ] || CONFIGFILE=$DEFAULT_CONFIGFILE
+[ -f "$CONFIGFILE" ] || {
+    echo "${prog}: Config file ${CONFIGFILE}: does not exist as a file"
+    false
+}
+configext=`echo $CONFIGFILE | sed -e 's/^.*\.//' | tr A-Z a-z`
+[ "$configext" = "json" -o "$configext" = "yml" ] || {
+    echo "${prog}:" Config file type not recognized by extension: $configext
+    false
+}
+configparent=`dirname $CONFIGFILE`
+configfile=`(cd $configparent; pwd)`/`basename $CONFIGFILE`
+VOLOPTS="$VOLOPTS -v ${configfile}:/app/people-config.${configext}:ro"
+ENVOPTS="-e OAR_PEOPLESERVER_PORT=$PORT -e OAR_PEOPLESERVER_CONFIG=/app/people-config.${configext}"
+
+[ -z "$STOREDIR" ] || {
+    [ -d "$STOREDIR" ] || {
+        parent=`dirname $STOREDIR`
+        if [ -d "$parent" ]; then
+            mkdir $STOREDIR
+        else
+            echo "${prog}: ${STOREDIR}: storage directory not found"
+            false
+        fi
+    }
+    sdir=`cd $STOREDIR; pwd`
+    VOLOPTS="$VOLOPTS -v ${sdir}:/data/nsd"
+    ENVOPTS="$ENVOPTS -e OAR_WORKING_DIR=/data/nsd"
+}
+
+NETOPTS=
+STOP_MONGO=true
+
+if [ -n "$USEMONGO" ]; then
+    DOCKER_COMPOSE="docker compose"
+    (docker compose version > /dev/null 2>&1) || DOCKER_COMPOSE=docker-compose
+    ($DOCKER_COMPOSE version  > /dev/null 2>&1) || {
+        echo ${prog}: docker compose required for -M
+        false
+    }
+
+    echo '+' source $dockerdir/peopleserver/mongo/mongo.env
+    source $dockerdir/peopleserver/mongo/mongo.env
+
+    # [ -n "$STOREDIR" -o "$ACTION" = "stop" ] || {
+    #     echo ${prog}: DIR argument must be provided with -M/--use-mongo
+    #     false
+    # }
+    if [ -n "$STOREDIR" ]; then
+        export OAR_MONGODB_DBDIR=`cd $STOREDIR; pwd`/mongo
+    else
+        export OAR_MONGODB_DBDIR=`mktemp --tmpdir -d _nsdmongo.XXXXX`
+    fi
+
+    NETOPTS="--network=mongo_default --link mongodb_server:mongodb"
+    ENVOPTS="$ENVOPTS -e OAR_MONGODB_HOST=mongodb -e OAR_MONGODB_USER=oarop"
+
+    [ "$ACTION" = "stop" ] || {
+        # now launch the database in its own containers
+        echo '+' $DOCKER_COMPOSE -f $dockerdir/peopleserver/mongo/docker-compose.mongo.yml up -d
+        $DOCKER_COMPOSE -f $dockerdir/peopleserver/mongo/docker-compose.mongo.yml up -d
+
+        echo 
+        echo NOTE:  Visit http://localhost:8081/ to view MongoDB contents
+        echo 
+    }
+
+    function stop_mongo {
+        echo '+' $DOCKER_COMPOSE -f $dockerdir/peopleserver/mongo/docker-compose.mongo.yml down
+        $DOCKER_COMPOSE -f $dockerdir/peopleserver/mongo/docker-compose.mongo.yml down
+    }
+    STOP_MONGO=stop_mongo
+fi
+
+CONTAINER_NAME="peopleserver"
+function stop_server {
+    echo '+' docker kill $CONTAINER_NAME
+    docker kill $CONTAINER_NAME
+}
+trap "{ stop_server; $STOP_MONGO; }" TERM STOP
+
+if [ "$ACTION" = "stop" ]; then
+    echo Shutting down the NSD server...
+    stop_server || true
+    $STOP_MONGO
+else
+    echo '+' docker run $ENVOPTS $VOLOPTS $NETOPTS -p 127.0.0.1:${PORT}:${PORT}/tcp --rm --name=$CONTAINER_NAME $DETACH $PACKAGE_NAME/peopleserver $DBTYPE
+    docker run $ENVOPTS $VOLOPTS $NETOPTS -p 127.0.0.1:${PORT}:${PORT}/tcp --rm --name=$CONTAINER_NAME $DETACH $PACKAGE_NAME/peopleserver $DBTYPE
+fi
+
---
+++ scripts/people-uwsgi.py
@@ -0,0 +1,72 @@
+"""
+the uWSGI script for launching the PDP web service.
+
+This script launches the web service using uwsgi.  For example, one can 
+launch the service with the following command:
+
+  uwsgi --plugin python3 --http-socket :9090 --wsgi-file pdp-uwsgi.py \
+        --set-ph oar_config_file=pdp_conf.yml --set-ph oar_working_dir=_test
+
+"""
+import os, sys, logging, copy
+from copy import deepcopy
+
+try:
+    import nistoar
+except ImportError:
+    oarpath = os.environ.get('OAR_PYTHONPATH')
+    if not oarpath and 'OAR_HOME' in os.environ:
+        oarpath = os.path.join(os.environ['OAR_HOME'], "lib", "python")
+    if oarpath:
+        sys.path.insert(0, oarpath)
+    import nistoar
+
+from nistoar.base import config
+from nistoar.nsd import wsgi, service
+
+try:
+    import uwsgi
+except ImportError:
+    # simulate uwsgi for testing purpose
+    from nistoar.testing import uwsgi
+    uwsgi = uwsgi.load()
+
+def _dec(obj):
+    # decode an object if it is not None
+    return obj.decode() if isinstance(obj, (bytes, bytearray)) else obj
+
+# determine where the configuration is coming from
+confsrc = _dec(uwsgi.opt.get("oar_config_file"))
+if confsrc:
+    cfg = config.resolve_configuration(confsrc)
+
+elif 'oar_config_service' in uwsgi.opt:
+    srvc = config.ConfigService(_dec(uwsgi.opt.get('oar_config_service')),
+                                _dec(uwsgi.opt.get('oar_config_env')))
+    srvc.wait_until_up(int(_dec(uwsgi.opt.get('oar_config_timeout', 10))),
+                       True, sys.stderr)
+    cfg = srvc.get(_dec(uwsgi.opt.get('oar_config_appname', 'nsd')))
+
+elif config.service:
+    config.service.wait_until_up(int(os.environ.get('OAR_CONFIG_TIMEOUT', 10)),
+                                 True, sys.stderr)
+    cfg = config.service.get(os.environ.get('OAR_CONFIG_APP', 'nsd'))
+
+else:
+    raise config.ConfigurationException("resolver: nist-oar configuration not provided")
+
+workdir = _dec(uwsgi.opt.get("oar_working_dir"))
+if workdir:
+    cfg['working_dir'] = workdir
+
+config.configure_log(config=cfg)
+
+log = logging.getLogger("NSD")
+psvc = service.MongoPeopleService(cfg.get('db_url'))
+try:
+    psvc.load(cfg.get('data'), log)
+except Exception as ex:
+    log.exception("Failed to load people data: %s", str(ex))
+
+application = wsgi.app(cfg)
+logging.info("NSD service ready")
