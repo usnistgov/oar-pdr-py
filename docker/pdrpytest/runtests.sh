de80133b1880b06e367efb838b9f4c3d812a66cf
RayPlante <raymond.plante@nist.gov>
2024-06-22 23:56:15 -0400

    environment/ci upgrade:    * pull in updates in oar-metadata    * update pdr-specific dependencies    * launch mongo server for unit-testing

---
+++ docker/pdrpytest/runtests.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+#
+function install {
+    scripts/setversion.sh
+    scripts/install.sh --prefix=/app/pdr || return 1
+    export OAR_HOME=/app/pdr
+    export PYTHONPATH=$OAR_HOME/lib/python
+    export OAR_LOG_DIR=$OAR_HOME/var/logs
+}
+
+function exitopwith { 
+    echo $2 > $1.exit
+    exit $2
+}
+
+
+
+cmd=$1
+case "$1" in
+    makedist)
+        shift
+        scripts/makedist "$@"
+        ;;
+    build)
+        scripts/setversion.sh
+        (cd python && python setup.py build)
+        ;;
+    testall)
+        install || {
+            echo "testall: Failed to install oar-pdr-py"
+            exitopwith testall 2
+        }
+        shift
+
+        # wrapper root shell should have already started mongodb
+        stat=0
+        scripts/testall "$@"; stat=$?
+
+        [ "$stat" != "0" ] && {
+            echo "testall: One or more test packages failed (last=$stat)"
+            echo NOT OK
+            exitopwith testall 3
+        }
+        # echo All OK
+        ;;
+    install)
+        install
+        python -c 'import nistoar.pdr, jq'
+        ;;
+    testshell)
+        # wrapper root shell should have already started mongodb
+        install
+        exec /bin/bash
+        ;;
+    shell)
+        exec /bin/bash
+        ;;
+    *)
+        echo Unknown command: $1
+        echo Available commands:  build makedist testall install shell
+        exit 100
+        ;;
+esac
+
+EXCODE=$?
+echo $EXCODE > $cmd.exit
+exit $EXCODE
