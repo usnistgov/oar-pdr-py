50817c2ed933ef752c7e8a5407c95704690c42f9
Ray Plante <raymond.plante@nist.gov>
2025-03-16 14:21:00 -0400

    nsd.sync add cli, wrapper script, unit test against real service

---
+++ python/nistoar/nsd/sync/cli.py
@@ -0,0 +1,117 @@
+"""
+a command-line interface to health checks.  The :py:func:`main` function provides the implementation
+"""
+import argparse, sys, os, re, logging, textwrap, traceback as tb
+from argparse import ArgumentParser
+
+import yaml
+
+from ....pdr import config
+from ...exceptions import ConfigurationException
+# from ...notify.service import TargetManager, NotificationService
+# from ...notify.cli import StdoutMailer, StdoutArchiver, Failure
+from ...notify.cli import Failure
+from .sync import NSDSyncer
+
+prog = re.sub(r'\.py$', '', os.path.basename(sys.argv[0]))
+
+def define_options(progname):
+    """
+    return an ArgumentParser instance that is configured with options
+    for the command-line interface.
+    """
+    description = "Retrieve organization and people data from the NIST Staff Directory, "
+                  "cache it to local disk, and trigger a reload of the DBIO's mirror database"
+    epilog = None
+    
+    parser = ArgumentParser(progname, None, description, epilog)
+
+    parser.add_argument('-d', '--output-dir', type=str, dest='datadir', metavar='DIR', default=None,
+                        htlp="the directory where the org and people data will be cached")
+    parser.add_argument('-c', '--config-file', type=str, dest='cfgfile', metavar='FILE', 
+                        help="a file containing the configuration to use.  The schema can be "+
+                             "either for NSD data, NSD web app, or the DBIO app configuration")
+    parser.add_argument('-C', '--config-url', type=str, dest='cfgurl', metavar='URL',
+                        help="a URL that the configuration should be retrieved from.  If not "+
+                             "provided, the OAR_CONFIG_URL environment variable will be "+
+                             "consulted.  This option is ignored if -c/--config-file is provided.")
+    parser.add_argument('-D', '--dbio-nsd-url', type=str, dest-'dbionsd', metavar='URL',
+                        help="the endpoint URL for the DBIO's staff directory service; if not "+
+                             "provided, the service will not be triggered")
+    parser.add_argument('-l' '--logfile', action='store', dest='logfile', type=str, metavar='FILE',
+                        help="write messages that normally go to standard error to FILE as well.  "+
+                             "is also specified, the messages will only go to the logfile")
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
+                        help="print more (debug) messages to standard error and/or the log file")
+    parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                        help="suppress all error and warning messages to standard error")
+    parser.add_argument('ous', metavar='OU', type=str, nargs='*', default=[],
+                        help="OU abbreviation(s) to restrict the people data to.  If not provided, "
+                             "all OUs will be included")
+    return parser
+
+def request_reload(dbionsdep):
+    """
+    trigger a reload
+    """
+    pass
+
+def main(progname, args):
+    """
+    fetch the staff data and trigger an upload to our mirror
+    """
+    parser = define_options(progname)
+    opts = parser.parse_args(args)
+
+    rootlog = logging.getLogger    level = (opts.verbose and logging.DEBUG) or logging.INFO
+    if opts.logfile:
+        # write messages to a log file
+        fmt = "%(asctime)s " + (opts.origin or prog) + ".%(name)s %(levelname)s: %(message)s"
+        hdlr = logging.FileHandler(opts.logfile)
+        hdlr.setFormatter(logging.Formatter(fmt))
+        hdlr.setLevel(logging.DEBUG)
+        rootlog.addHandler(hdlr)
+        rootlog.setLevel(level)
+
+    # configure a default log handler
+    if not opts.quiet:
+        fmt = prog + ": %(levelname)s: %(message)s"
+        hdlr = logging.StreamHandler(sys.stderr)
+        hdlr.setFormatter(logging.Formatter(fmt))
+        hdlr.setLevel(logging.DEBUG)
+        rootlog.addHandler(hdlr)
+        rootlog.setLevel(level)
+    elif not rootlog.handlers:
+        rootlog.addHandler(logging.NullHandler())
+
+    # look for a provided configuration file
+    cfg = {}
+    if opts.cfgfile:
+        try:
+            cfg = read_config(opts.cfgfile)
+        except EnvironmentError as ex:
+            raise Failure("problem reading config file, {0}: {1}"
+                          .format(opts.cfgfile, ex.strerror))
+    else:
+        cfg = config.service.get("midas-dbio")
+
+    if cfg.get('services', {}).get('nsd'):
+        cfg = cfg['services']['nsd']
+    if cfg.get('data'):
+        cfg = cfg['data']
+
+    try:
+        syncer = NSDSyncer(cfg)
+        syncer.cached_data(opts.datadir)
+
+        if opts.dbionsd:
+            request_reload(opts.dbionsd)
+
+    except ConfigurationException as ex:
+        raise Failure(str(ex))
+    except NSDException as ex:
+        raise Failure("Failed to update NSD data: "+str(ex), 2)
+
+    
+        
+        
