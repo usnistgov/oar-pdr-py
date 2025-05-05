6353d7260d5de9ee7a77678e3f78c74bf9ba7e14
Ray Plante <raymond.plante@nist.gov>
2025-05-05 11:19:33 -0400

    dbio.notifier: remove logging configuration, tone down logging

--- python/nistoar/midas/dbio/notifier.py
+++ python/nistoar/midas/dbio/notifier.py
@@ -3,7 +3,6 @@ import websockets
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 class Notifier:
@@ -20,19 +19,16 @@ class Notifier:
         Asynchronously sends a notification message via WebSocket.
         :param str message: The message to send.
         """
-        logger.info(f"Starting....")
         try:
             loop = asyncio.get_running_loop()
-            logger.info(f"Obtained running event loop: {loop}")
         except RuntimeError:
             # No running event loop; create a new one
-            logger.warning("No running event loop found. Creating a new event loop.")
+            logger.debug("No running event loop found. Creating a new event loop.")
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            logger.info(f"New event loop created: {loop}")
 
         if not loop.is_running():
-            logger.warning("Event loop is not running. Starting the event loop.")
+            logger.debug("Event loop is not running. Starting the event loop.")
             loop.run_until_complete(self._send_notification(message))
         else:
             future = asyncio.run_coroutine_threadsafe(self._send_notification(message), loop)
@@ -43,7 +39,7 @@ class Notifier:
         Coroutine to send a notification message via WebSocket.
         :param str message: The message to send.
         """
-        logger.info(f"Connecting to WebSocket server at {self.uri}...")
+        logger.debug(f"Connecting to WebSocket server at {self.uri}...")
         try:
             async with websockets.connect(self.uri) as websocket:
                 if self.api_key:
@@ -51,9 +47,9 @@ class Notifier:
                     logger.info(f"Sending WebSocket message: {message}")
                     authentified_message = f"{self.api_key},{message}"
                     await websocket.send(authentified_message)
-                    logger.info("WebSocket message sent successfully.")
+                    logger.debug("WebSocket message sent successfully.")
                     
                 await websocket.close()
-                logger.info("WebSocket connection closed cleanly.")
+                logger.debug("WebSocket connection closed cleanly.")
         except Exception as e:
             logger.error(f"Error sending WebSocket message: {e}")
