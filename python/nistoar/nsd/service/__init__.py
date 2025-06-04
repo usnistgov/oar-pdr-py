b5e1f137b4a6dad9fb5353fba80a044cd69f9ba6
Ray Plante <raymond.plante@nist.gov>
2025-06-04 11:06:56 -0400

    nistoar.nsd: restructure service to add file-based implementation

---
+++ python/nistoar/nsd/service/__init__.py
@@ -0,0 +1,32 @@
+"""
+module providing implementations of an staff directory service.  A 
+:py:class:`~nistoar.nsd.service.base.PeopleService` speaks directly to a backend storage system
+to return staff and organization metadata.  The :py:mod:`.wsgi module<nistoar.nsd.wsgi>` is responsible 
+for exposing a service through a web interface.  
+"""
+from .base import PeopleService
+from .mongo import MongoPeopleService
+
+def create_people_service(config):
+    """
+    instantiate a :py:class:`PeopleService` instance based on the given configuration
+    """
+    if not isinstance(config, Mapping):
+        raise ConfigurationException("people_service config: not a dictionary: "+str(config))
+
+    if config.get("factory") == "mongo":
+        dburl = config.get("db_url")
+        if not dburl:
+            raise ConfigurationException("Missing required config param: people_service.db_url")
+        # logging.getLogger("nsd.create_people_service").debug("Creating a MongoPeopleService")
+        return MongoPeopleService(dburl)
+
+    elif config.get("factory") == "files":
+        from .files import FileBasedPeopleService
+        return FileBasedPeopleService(config)
+
+    elif config.get("factory"):
+        raise ConfigurationException("people_service.factory type not supported: "+config["factory"])
+
+    return None
+
