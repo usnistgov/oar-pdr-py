b3fe74f19a8bcbd84c753a8b66a117a34bb403d3
Ray Plante <raymond.plante@nist.gov>
2025-06-20 11:18:40 -0400

    dap.extrev: refine API specs

--- python/nistoar/midas/dap/extrev/__init__.py
+++ python/nistoar/midas/dap/extrev/__init__.py
@@ -28,7 +28,7 @@ class ExternalReviewClient(ABC):
         self.cfg = config or {}
 
     @abstractmethod
-    def submit(self, id: str, version: str=None, **options=None):
+    def submit(self, id: str, submitter: str, version: str=None, **options=None):
         """
         submit a specified DAP to this system for review.  The options supported depend on 
         the implementation (and implementations should ignore any option parameters that 
@@ -41,8 +41,20 @@ class ExternalReviewClient(ABC):
              (*[str]*) provided when a DAP revision is being requested for review, this is a 
              list of statements indicating what has changed since the previous publication.
         ``reviewers``
-             (*[Mapping]*) a list of designations of reviewers requested to be included 
+             (*[dict]*) a list of designations of reviewers requested to be included 
              among the full set of assigned reviewers.  
+        ``security_review``
+             (*bool*) if True, this DAP includes content (e.g. software) that requires 
+             review IT security review.  
+
+        :param str id:         the identifier for the DAP to submit to the review system
+        :param str submitter:  the identifier for the user submitting the DAP (this is 
+                               usually the owner of the record).
+        :param str version:    the version of the DAP being submitted.  This should be 
+                               provided when revising a previously published DAP; otherwise,
+                               the implementation may assume that the initial version is being 
+                               submitted.  
+        :param Mapping options:  extra implementation-specific keyword parameters
         """
         raise NotImplemented()
 
--- python/nistoar/midas/dap/extrev/nps1.py
+++ python/nistoar/midas/dap/extrev/nps1.py
@@ -8,6 +8,18 @@ class NPSExternalReviewClient(ExternalReviewClient):
     """
     an ExternalReviewClient implementation that connects to the legacy NPS service
     (consistent with MIDAS v3).  
+
+    This implementation will look for the following parameters from the configuration 
+    dictionary provided at construction time:
+
+    ``draft_url_template``
+        (*str*) *required*. a string template for forming a URL where a reviewer can view the draft 
+        landing page for the DAP.  The template should include one "%s" insert point where the draft 
+        DAP ID can be inserted.
+    ``published_url_template``
+        (*str*) *required*. a string template for forming a URL where the DAP will be viewable once 
+        it is published.  The template should include one "%s" insert point where the public 
+        DAP ID can be inserted.
     """
 
     def __init__(self, config):
@@ -16,8 +28,64 @@ class NPSExternalReviewClient(ExternalReviewClient):
         """
         super(NPSExternalReviewClient, self).__init__(config)
 
-        # get templates for the home page URL
+        # get templates for the home page URL from config
+        self._drafturl_tmpl = get.get("draft_url_template")
+        if not self._drafturl_tmpl:
+            raise ConfigurationException("Missing required config param: draft_url_template")
 
-    def submit(self, id: str, version: str=None, **options=None):
+        self._puburl_tmpl = self.cfg.get("published_url_template")
+        if not self._drafturl_tmpl:
+            raise ConfigurationException("Missing required config param: published_url_template")
+
+    def submit(self, id: str, submitter: str, version: str=None, **options=None):
+        """
+        submit a specified DAP to this system for review.  This implementation supports the 
+        following extra options:
+        ``title``
+             (*str*) the title assigned to the DAP (for display purposes)
+        ``pubid``
+             (*str*) the identifier that will be assigned to the DAP once it is published
+        ``instructions``
+             (*[str]*) a list of statements that should be passed as special instructions to 
+             the reviewers.
+        ``changes``
+             (*[str]*) a list of statements or phrases describing the reason for review.  
+        ``reviewers``
+             (*[dict]*) a list of designations of reviewers requested to be included 
+             among the full set of assigned reviewers.  
+        ``security_review``
+             (*bool*) if True, this DAP includes content (e.g. software) that requires 
+             review IT security review.  
+
+        :param str id:         the identifier for the DAP to submit to the review system
+        :param str submitter:  the identifier for the user submitting the DAP (this is 
+                               usually the owner of the record).
+        :param str version:    the version of the DAP being submitted.  This should be 
+                               provided when revising a previously published DAP; otherwise,
+                               the implementation may assume that the initial version is being 
+                               submitted.  
+        :param Mapping options:  extra implementation-specific keyword parameters
+        """
         pass
+
+    _review_reasons = {
+        "NEWREC":  "New Record",
+        "MDUP":    "Metadata Update",
+        "MINDATA": "Data change (minor)",
+        "MAJDATA": "Data change (major)",
+        "NEWFILE": "New file addition",
+        "RMFILE":  "Distribution removal"
+    ]
+
+    def select_review_reason(self, changes: List[str] = None, version: str = None) -> str:
+        """
+        Return the string to use as the review reason
+        """
+        if not changes:
+            return self._review_reasons['NEWREC']
+
+        # This part depends on the DAP tool phrases used;
+
+        # default to assuming this is a major change
+        return self._review_reasons['MAJDATA']
 
