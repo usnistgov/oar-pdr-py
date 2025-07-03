cce71ce443694879e88376523f0eadaaa9275407
MelvinMartins-NIST <melvin.martins@nist.gov>
2025-07-03 15:06:37 -0400

    Initial implement NPS client and unit tests

--- python/nistoar/midas/dap/extrev/__init__.py
+++ python/nistoar/midas/dap/extrev/__init__.py
@@ -28,33 +28,32 @@ class ExternalReviewClient(ABC):
         self.cfg = config or {}
 
     @abstractmethod
-    def submit(self, id: str, submitter: str, version: str=None, **options=None):
+    def submit(self, id: str, submitter: str, version: str=None, **options):
         """
-        submit a specified DAP to this system for review.  The options supported depend on 
-        the implementation (and implementations should ignore any option parameters that 
-        it does not recognize; however, support for the following parameters are defined as 
+        submit a specified DAP to this system for review.  The options supported depend on
+        the implementation (and implementations should ignore any option parameters that
+        it does not recognize; however, support for the following parameters are defined as
         follows:
         ``instructions``
-             (*[str]*) a list of statements that should be passed as special instructions to 
+             (*[str]*) a list of statements that should be passed as special instructions to
              the reviewers.
         ``changes``
-             (*[str]*) provided when a DAP revision is being requested for review, this is a 
+             (*[str]*) provided when a DAP revision is being requested for review, this is a
              list of statements indicating what has changed since the previous publication.
         ``reviewers``
-             (*[dict]*) a list of designations of reviewers requested to be included 
-             among the full set of assigned reviewers.  
+             (*[dict]*) a list of designations of reviewers requested to be included
+             among the full set of assigned reviewers.
         ``security_review``
-             (*bool*) if True, this DAP includes content (e.g. software) that requires 
-             review IT security review.  
+             (*bool*) if True, this DAP includes content (e.g. software) that requires
+             review IT security review.
 
         :param str id:         the identifier for the DAP to submit to the review system
-        :param str submitter:  the identifier for the user submitting the DAP (this is 
+        :param str submitter:  the identifier for the user submitting the DAP (this is
                                usually the owner of the record).
-        :param str version:    the version of the DAP being submitted.  This should be 
+        :param str version:    the version of the DAP being submitted.  This should be
                                provided when revising a previously published DAP; otherwise,
-                               the implementation may assume that the initial version is being 
-                               submitted.  
+                               the implementation may assume that the initial version is being
+                               submitted.
         :param Mapping options:  extra implementation-specific keyword parameters
         """
         raise NotImplemented()
-
