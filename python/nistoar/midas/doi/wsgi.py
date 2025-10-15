0a3041a332f119721ea5c2f3f962e1ad6bafbae8
Ray Plante <raymond.plante@nist.gov>
2025-10-15 18:22:27 -0400

    add midas.doi.wsgi and initial unit test

---
+++ python/nistoar/midas/doi/wsgi.py
@@ -0,0 +1,147 @@
+"""
+A RESTful web service that resolves a DOI to a NERDm reference
+"""
+import logging, re
+from logging import Logger
+from collections import OrderedDict
+from typing import List, Mapping, Callable
+
+from nistoar.nerdm.convert.doi import DOIResolver
+from nistoar.doi import DOIDoesNotExist, DOIClientException, DOIResolutionException
+from nistoar.web.rest import ServiceApp, Handler, HandlerWithJSON, Ready
+from nistoar.base.config import ConfigurationException
+from nistoar.nerdm.constants import BIB_SCHEMA_URI
+from nistoar.pdr.utils.prov import Agent
+from ..dbio.wsgi import DBIOHandler
+from .. import system
+
+deflog = logging.getLogger(system.system_abbrev)   \
+                .getChild('doi').getChild('wsgi')
+
+DEF_BASE_PATH = '/'
+
+_doi_pat = re.compile("(doi:)?(10\.\d+/[^#\s]+)")
+_doi_baseurl_pat = re.compile("^https?://(dx\.)?doi\.org/")
+
+class DOI2NERDmHandler(HandlerWithJSON):
+    """
+    a base handler for converting DOIs to NERDm metadata
+    """
+    def __init__(self, doiresolver, path: str, wsgienv: dict, start_resp: Callable, who=None, 
+                 log: Logger=None, app=None):
+        super(DOI2NERDmHandler, self).__init__(path, wsgienv, start_resp, who, {}, log, app)
+        self._doires = doiresolver
+
+    def send_reference(self, doi, ashead=False):
+        """
+        Convert a DOI to a NERDm Reference object and send it to the client
+
+        Note that if the given DOI is found not to exist, the returned object will 
+        contain the DOI (in the ``@id`` property) but with no other metadata (such 
+        as title, location, citation text, etc.).
+
+        :param str doi:  the DOI to convert, stripped of its prefix and component identifier; 
+                         that is, it must begin with "10.".  
+        :param str ashead:  send a response appropriate for a HEAD request
+        """
+        try:
+
+            ref = self._doires.to_reference(doi)
+            
+        except DOIDoesNotExist as ex:
+            
+            ref = OrderedDict([
+                ("@id", "doi:"+doi),
+                ("_extensionSchemas",
+                 [ BIB_SCHEMA_URI+"#/definitions/DCiteReference" ]),
+                ("pdr:comment", "DOI does not exist yet")
+            ])
+
+        return self.send_json(ref, ashead=ashead)
+
+    def send_authors(self, doi, ashead=False):
+        """
+        convert the given DOI to a list of NERDm authors (People objects) and send it to the client
+
+        :param str doi:  the DOI to convert, stripped of its prefix and component identifier; 
+                         that is, it must begin with "10.".  
+        :param str ashead:  send a response appropriate for a HEAD request
+        """
+        auths = self._doires.to_authors(doi)
+        return self.send_json(auths, ashead=ashead)
+
+    def do_GET(self, path, ashead=False):
+        """
+        Convert a DOI to the requested NERDm object
+        """
+        if not path:
+            return self.send_error_obj(405, "Method Not Allowed"
+                                       "GET request must include a NERDm type, authors or ref",
+                                       ashead=ashead)
+
+        parts = path.strip('/').split('/', 1)
+        path = parts[1] if len(parts) > 1 else ""
+        outtype = parts[0]
+
+        if outtype not in ["ref", "authors"]:
+            return self.send_error_obj(404, "Not Found",
+                                       "Not a supportted NERDm output type", ashead=ashead)
+        if not path:
+            return self.send_error_obj(405, "Method Not Allowed"
+                                       "GET request must include a NERDm type, authors or ref",
+                                       ashead=ashead)
+
+        # interpret path as a DOI 
+        path = _doi_baseurl_pat.sub('', path)  # strip https://doi.org/
+        m = _doi_pat.search(path)              # confirm legal DOI 
+        if not m:
+            return self.send_error_obj(400, "Not a DOI",
+                                       "Identifier not recognized as a DOI",
+                                       ashead=ashead)
+        doi = m.group(2)
+        try:
+            if outtype == "authors":
+                return self.send_authors(doi)
+
+            # should be "ref" but we can allow for synonyms; see outtype check above
+            return self.send_reference(doi)
+                                  
+        except DOIClientException as ex:
+            self.log.error("Problem using DOI resolver (prog error?) %s: %s",
+                           doi, str(ex))
+            return self.send_error_obj(500, "Server Error",
+                                       "Server error while resolving OOI",
+                                       ashead=ashead)
+        
+        except DOIResolutionException as ex:
+            self.log.error("Unexpected DOI resolution response to %s: %s",
+                           doi, str(ex))
+            return self.send_error_obj(503, "DOI Resolver Failure",
+                                       "Trouble accessing remote DOI resolver service",
+                                       ashead=ashead)
+
+class DOI2NERDmApp(ServiceApp):
+    """
+    a web app for converting a DOI to NERDm metadata
+    """
+
+    def __init__(self, log: Logger, config: dict={}):
+        super(DOI2NERDmApp, self).__init__("doi2nerdm", log, config)
+        if not self.cfg.get("doi_resolver"):
+            raise ConfigurationException("DOI2NERDmApp: missing required parameter: doi_resolver")
+        self._doires = DOIResolver.from_config(self.cfg["doi_resolver"])
+
+    def create_handler(self, env: dict, start_resp: Callable, path: str, who: Agent) -> Handler:
+        """
+        return a handler instance to handle a particular request to a path
+        :param Mapping env:  the WSGI environment containing the request
+        :param Callable start_resp:  the start_resp function to use initiate the response
+        :param str    path:  the path to the resource being requested.  This is usually 
+                             relative to a parent path that this ServiceApp is configured to 
+                             handle.  
+        :param Agent   who:  the authenticated user agent making the request
+        """
+        if not path:
+            return Ready(path, env, start_resp, who, log=self.log, app=self)
+
+        return DOI2NERDmHandler(self._doires, path, env, start_resp, who, self.log, self)
---
+++ python/tests/nistoar/midas/doi/test_wsgi.py
@@ -0,0 +1,222 @@
+import os, json, pdb, logging, tempfile
+from collections import OrderedDict
+from io import StringIO
+from pathlib import Path
+import unittest as test
+from unittest.mock import Mock
+
+from nistoar.midas.doi import wsgi as doim
+from nistoar.doi.resolving import DOIInfo, DOIDoesNotExist, DOIResolverError
+from nistoar.nerdm.convert.doi import DOIResolver
+import nistoar.doi.resolving.common as res
+
+tmpdir = tempfile.TemporaryDirectory(prefix="_test_project.")
+loghdlr = None
+rootlog = None
+def setUpModule():
+    global loghdlr
+    global rootlog
+    rootlog = logging.getLogger()
+    rootlog.setLevel(logging.DEBUG)
+    loghdlr = logging.FileHandler(os.path.join(tmpdir.name,"test_nsd.log"))
+    loghdlr.setLevel(logging.DEBUG)
+    loghdlr.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+    rootlog.addHandler(loghdlr)
+
+def tearDownModule():
+    global loghdlr
+    if loghdlr:
+        if rootlog:
+            rootlog.removeHandler(loghdlr)
+            loghdlr.flush()
+            loghdlr.close()
+        loghdlr = None
+    tmpdir.cleanup()
+
+sampleref = {
+  "@id": "doi:10.10/XXX",
+  "@type": [
+    "schema:Article"
+  ],
+  "refType": "IsCitedBy",
+  "title": "Ecological traits of the world\\u2019s primates",
+  "issued": "2019-05-13",
+  "location": "https://goober.org/10.10/XXX",
+  "citation": "ibid",
+  "_extensionSchemas": [
+    "https://data.nist.gov/od/dm/nerdm-schema/bib/v0.7#/definitions/DCiteReference"
+  ]
+}
+
+sampleauths = [
+  {
+    "@type": "foaf:Person",
+    "familyName": "Galen Acedo",
+    "givenName": "Carmen",
+    "fn": "Carmen Galen Acedo"
+  },
+  {
+    "@type": "foaf:Person",
+    "familyName": "Arroyo",
+    "givenName": "Victor",
+    "fn": "Victor Arroyo",
+    "orcid": "0000-0002-0858-0324",
+    "affiliation": [
+      {
+        "title": "The Institute",
+        "@type": "schema:affiliation"
+      }
+    ]
+  },
+  {
+    "@type": "foaf:Person",
+    "familyName": "Andresen",
+    "givenName": "Ellen",
+    "fn": "Ellen Andresen"
+  },
+  {
+    "@type": "foaf:Person",
+    "familyName": "Arasa-Gisbert",
+    "givenName": "Ricard",
+    "fn": "Ricard Arasa-Gisbert"
+  }
+]
+
+class TestDOI2NERDmHandler(test.TestCase):
+
+    def start(self, status, headers=None, extup=None):
+        self.resp.append(status)
+        for head in headers:
+            self.resp.append("{0}: {1}".format(head[0], head[1]))
+
+    def body2data(self, body):
+        return json.loads("\n".join(self.tostr(body)), object_pairs_hook=OrderedDict)
+
+    def tostr(self, resplist):
+        return [e.decode() for e in resplist]
+
+    def setUp(self):
+        res._client_info = None
+        self.cfg = {
+            "doi_resolver": {
+                "app_name": "NIST Public Data Repository: pubserver (oar-pdr)",
+                "app_version": "1.5+",
+                "app_url": "https://data.nist.gov/",
+                "email": "datasupport@nist.gov"
+            }
+        }
+        self.resolver = DOIResolver.from_config(self.cfg)
+        self.resolver.to_reference = Mock(return_value=sampleref)
+        self.resolver.to_authors = Mock(return_value=sampleauths)
+        self.resp = []
+        
+    def tearDown(self):
+        res._client_info = None
+
+    def test_send_reference(self):
+        doi = "10.88888/goober"
+        path = f"ref/{doi}/"
+        req = {
+            "REQUEST_METHOD": "GET",
+            "PATH_INFO": path
+        }
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.send_reference(doi)
+        self.assertIn("200 ", self.resp[0])
+        ref = self.body2data(body)
+        self.assertEqual(ref['@type'], ['schema:Article'])
+        self.assertEqual(ref['@id'], 'doi:10.10/XXX')
+        self.assertEqual(ref['refType'], 'IsCitedBy')
+        self.assertEqual(ref['title'],
+                         "Ecological traits of the world\\u2019s primates")
+        self.assertEqual(ref['location'], "https://goober.org/10.10/XXX")
+        self.assertEqual(ref['issued'], '2019-05-13')
+        self.assertEqual(ref['citation'], 'ibid')
+
+        def donotfind(*args, **kw):
+            raise DOIDoesNotExist(doi)
+        self.resolver.to_reference = Mock(side_effect=donotfind)
+        self.resp = []
+        body = hdlr.send_reference(doi)
+        self.assertIn("200 ", self.resp[0])
+        ref = self.body2data(body)
+        self.assertEqual(ref['@id'], 'doi:10.88888/goober')
+        self.assertNotIn("@type", ref)
+        self.assertNotIn("title", ref)
+        self.assertNotIn("location", ref)
+
+        self.resolver.to_reference = Mock(side_effect=DOIResolverError)
+        with self.assertRaises(DOIResolverError):
+            hdlr.send_reference(doi)
+
+    def test_do_GET_ref(self):
+        doi = "10.88888/goober"
+        path = "ref/"+doi
+        req = {
+            "REQUEST_METHOD": "GET",
+            "PATH_INFO": path
+        }
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("200 ", self.resp[0])
+        ref = self.body2data(body)
+        self.assertEqual(ref['@type'], ['schema:Article'])
+        self.assertEqual(ref['@id'], 'doi:10.10/XXX')
+        self.assertEqual(ref['refType'], 'IsCitedBy')
+        self.assertEqual(ref['title'],
+                         "Ecological traits of the world\\u2019s primates")
+        self.assertEqual(ref['location'], "https://goober.org/10.10/XXX")
+        self.assertEqual(ref['issued'], '2019-05-13')
+        self.assertEqual(ref['citation'], 'ibid')
+
+        self.resp = []
+        path = "ref/doi:"+doi
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("200 ", self.resp[0])
+        ref2 = self.body2data(body)
+        self.assertEqual(ref2, ref)
+
+        self.resp = []
+        path = "ref/https://doi.org/"+doi
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("200 ", self.resp[0])
+        ref2 = self.body2data(body)
+        self.assertEqual(ref2, ref)
+
+        self.resp = []
+        path = "ref/http://dx.doi.org/"+doi
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("200 ", self.resp[0])
+        ref2 = self.body2data(body)
+        self.assertEqual(ref2, ref)
+
+        self.resp = []
+        path = "ref/ark:/18434/mds2-5801"
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("400 ", self.resp[0])
+        err = self.body2data(body)
+        self.assertIn('oar:message', err)
+
+        def failure(*args, **kw):
+            raise DOIResolverError()
+        self.resp = []
+        self.resolver.to_reference = Mock(side_effect=failure)
+        path = "ref/http://dx.doi.org/"+doi
+        hdlr = doim.DOI2NERDmHandler(self.resolver, path, req, self.start, log=rootlog)
+        body = hdlr.handle()
+        self.assertIn("503 ", self.resp[0])
+        err = self.body2data(body)
+        self.assertIn('oar:message', err)
+
+
+        
+
+
+        
+                         
+if __name__ == '__main__':
+    test.main()
